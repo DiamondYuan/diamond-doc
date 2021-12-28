b4d732f53b9f1a97c6311e449f298441dab06875
DiamondYuan <admin@diamondyuan.com>
2021-12-29 00:45:59 +0800

    feat: add getOrCreateFromMap

---
+++ src/utils/get-or-create.ts
@@ -0,0 +1,15 @@
+export function getOrCreateFromMap<T>(
+  map: Map<string, Map<string, unknown>>,
+  first: string,
+  second: string,
+  init: (first: string, second: string) => unknown
+): T {
+  const firstMap = map.get(first) || new Map<string, unknown>();
+  map.set(first, firstMap);
+  let value = firstMap.get(second) as T | undefined;
+  if (!value) {
+    value = init(first, second) as T;
+  }
+  firstMap.set(second, value);
+  return firstMap.get(second) as T;
+}
