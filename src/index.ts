25cd085a3f04bb711aa10873c228f616108c8308
DiamondYuan <admin@diamondyuan.com>
2022-01-04 21:25:00 +0800

    fix: export constants

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { DiamondDoc } from "./doc/DiamondDoc";
 export { DiamondArray } from "./structure/DiamondArray";
 export { DiamondMap } from "./structure/DiamondMap";
 export * from "./types";
+export * from "./constants";
