50d048733b1b4d92848b910054a9cc571babd66d
DiamondYuan <admin@diamondyuan.com>
2022-01-12 00:31:28 +0800

    feat: update benchmark

--- test/benchmark/array.ts
+++ test/benchmark/array.ts
@@ -4,22 +4,6 @@ import fs from "fs/promises";
 import zlib from "zlib";
 import assert from "assert";
 
-// const data: number[] = [];
-// for (let i = 0; i < 100000; i++) {
-//   const a1 = Math.floor(Math.random() * i);
-//   data.push(a1);
-// }
-
-// array.push("");
-// const start = Date.now();
-// data.forEach((e) => {
-//   array.insert(e, `${e}`);
-// });
-// const time = Date.now() - start;
-// console.log(time);
-
-// console.log(array.toJS());
-
 const run = async () => {
   const dataPath = path.join(__dirname, "data/automerge-paper.json.gz");
   const json = JSON.parse(
@@ -36,15 +20,21 @@ const run = async () => {
     }[];
   };
   let content = json.startContent;
+
+  for (const { patches } of json.txns) {
+    for (const [pos, delHere, insContent] of patches) {
+      const before = content.slice(0, pos);
+      const after = content.slice(pos + delHere);
+      content = before + insContent + after;
+    }
+  }
+
   const doc = new TestDoc();
   const array = doc.getArray("benchmark");
   array.push("");
   console.time();
   for (const { patches } of json.txns) {
     for (const [pos, delHere, insContent] of patches) {
-      const before = content.slice(0, pos);
-      const after = content.slice(pos + delHere);
-      content = before + insContent + after;
       if (delHere) {
         array.remove(pos + delHere);
       } else {
@@ -53,7 +43,6 @@ const run = async () => {
     }
   }
   console.timeEnd();
-  console.log(array.toJS().join("").length, content.length);
   assert(json.endContent === content, "equal");
   assert(array.toJS().join("") === content, "equal");
 };
