942f2404c42c352804e81e8f0a45993bd1cfe4dc
DiamondYuan <admin@diamondyuan.com>
2022-01-12 00:30:09 +0800

    feat: add benchmark

--- test/benchmark/array.ts
+++ test/benchmark/array.ts
@@ -1,20 +1,61 @@
 import { TestDoc } from "../fixture/test-doc";
+import path from "path";
+import fs from "fs/promises";
+import zlib from "zlib";
+import assert from "assert";
 
-const data: number[] = [];
-for (let i = 0; i < 10000; i++) {
-  const a1 = Math.floor(Math.random() * i);
-  data.push(a1);
-}
+// const data: number[] = [];
+// for (let i = 0; i < 100000; i++) {
+//   const a1 = Math.floor(Math.random() * i);
+//   data.push(a1);
+// }
 
-const doc = new TestDoc();
+// array.push("");
+// const start = Date.now();
+// data.forEach((e) => {
+//   array.insert(e, `${e}`);
+// });
+// const time = Date.now() - start;
+// console.log(time);
 
-const array = doc.getArray("benchmark");
-array.push("");
-const start = Date.now();
-data.forEach((e) => {
-  array.insert(e, `${e}`);
-});
-const time = Date.now() - start;
-console.log(time);
+// console.log(array.toJS());
 
-console.log(array.toJS());
+const run = async () => {
+  const dataPath = path.join(__dirname, "data/automerge-paper.json.gz");
+  const json = JSON.parse(
+    zlib.gunzipSync(await fs.readFile(dataPath)).toString()
+  ) as {
+    startContent: string;
+    endContent: string;
+    txns: {
+      time: string;
+      /**
+       * like [position, numDeleted, inserted content]
+       */
+      patches: Array<[number, number, number]>;
+    }[];
+  };
+  let content = json.startContent;
+  const doc = new TestDoc();
+  const array = doc.getArray("benchmark");
+  array.push("");
+  console.time();
+  for (const { patches } of json.txns) {
+    for (const [pos, delHere, insContent] of patches) {
+      const before = content.slice(0, pos);
+      const after = content.slice(pos + delHere);
+      content = before + insContent + after;
+      if (delHere) {
+        array.remove(pos + delHere);
+      } else {
+        array.insert(pos, insContent);
+      }
+    }
+  }
+  console.timeEnd();
+  console.log(array.toJS().join("").length, content.length);
+  assert(json.endContent === content, "equal");
+  assert(array.toJS().join("") === content, "equal");
+};
+
+run();
---
+++ test/benchmark/data/LICENSE.md
@@ -0,0 +1,7 @@
+content of license.md is copy from [josephg/crdt-benchmarks](https://github.com/josephg/crdt-benchmarks/blob/161cf7129fc4da4780bc2230cf844fab456501db/README.md)
+
+## automerge-paper.json.gz
+
+This data set is copied from the automerge-perf repository, prepared by Martin Kleppmann. It contains the editing trace from writing this LaTeX paper. The trace contains 182,315 inserts and 77,463 deletes.
+
+Due to the editor in which the paper was written, each txn contains exactly 1 patch, which is either a single character insert or single character delete. Also there are no timestamps, so it looks like every edit happened at the same moment.
