6708e60ff103590c54e00e85a0d01ca69229965a
DiamondYuan <admin@diamondyuan.com>
2022-01-11 22:59:04 +0800

    test: add test for redo and undo

--- test/undo.spec.ts
+++ test/undo.spec.ts
@@ -1,19 +1,19 @@
 import { TestDoc } from "./fixture/test-doc";
 
-import { EditStackService } from '../src/undo'
+import { EditStackService } from "../src/undo";
 
 it("test map", () => {
-  const doc = new TestDoc([], 'a');
+  const doc = new TestDoc([], "a");
   const undoManager = doc.createOperationManager(EditStackService);
   const map = doc.getMap();
   undoManager.track(map);
   map.set("a", "0");
   expect(undoManager.canUndo()).toBe(true);
   undoManager.undo();
-  expect(doc.version).toEqual({ a: 3 })
+  expect(doc.version).toEqual({ a: 3 });
   expect(map.get("a")).toBe(undefined);
   undoManager.redo();
-  expect(doc.version).toEqual({ a: 4 })
+  expect(doc.version).toEqual({ a: 4 });
   expect(map.get("a")).toBe("0");
 });
 
@@ -21,41 +21,49 @@ it("test array", () => {
   const doc = new TestDoc();
   const remote = new TestDoc();
   const undoManager = doc.createOperationManager(EditStackService);
-  const array = doc.getArray('arr');
+  const array = doc.getArray("arr");
   undoManager.track(array);
-  array.push('1')
-  remote.merge(doc)
-  remote.getArray('arr').push('2')
-  remote.getArray('arr').remove(0)
-  doc.merge(remote)
-  array.push('3')
-  undoManager.pushStackElement()
-  array.push('4')
-  expect(array.toJS().join('')).toBe('234')
+  array.push("1");
+  remote.merge(doc);
+  remote.getArray("arr").push("2");
+  remote.getArray("arr").remove(0);
+  doc.merge(remote);
+  array.push("3");
+  undoManager.pushStackElement();
+  array.push("4");
+  expect(array.toJS().join("")).toBe("234");
   undoManager.undo();
-  expect(array.toJS().join('')).toBe('23')
+  expect(array.toJS().join("")).toBe("23");
   undoManager.undo();
-  expect(array.toJS().join('')).toBe('2')
+  expect(array.toJS().join("")).toBe("2");
   undoManager.redo();
-  expect(array.toJS().join('')).toBe('23')
+  expect(array.toJS().join("")).toBe("23");
   undoManager.redo();
-  expect(array.toJS().join('')).toBe('234')
+  expect(array.toJS().join("")).toBe("234");
 });
 
-
-it('array', () => {
+it("test canRedo and canUndo", () => {
   const doc = new TestDoc();
   const undoManager = doc.createOperationManager(EditStackService);
-  const array = doc.getArray('arr');
-  array.push('0');
-  undoManager.track(array)
-  array.push('1');
-  array.remove(0)
+  const array = doc.getArray("arr");
+  array.push("0");
+  undoManager.track(array);
+  array.push("1");
+  array.remove(0);
   undoManager.undo();
-  expect(array.toJS().join('')).toBe('0')
-  expect(undoManager.canRedo()).toBeTruthy()
-  expect(undoManager.canUndo()).toBeFalsy()
-  array.push('2')
-  expect(undoManager.canUndo()).toBeTruthy()
-  expect(undoManager.canRedo()).toBeFalsy()
-})
+  expect(array.toJS().join("")).toBe("0");
+  expect(undoManager.canRedo()).toBeTruthy();
+  expect(undoManager.canUndo()).toBeFalsy();
+  var version = doc.version;
+  undoManager.undo();
+  expect(doc.version).toEqual(version);
+  array.push("2");
+  expect(undoManager.canUndo()).toBeTruthy();
+  expect(undoManager.canRedo()).toBeFalsy();
+  var version = doc.version;
+  undoManager.canRedo();
+  expect(doc.version).toEqual(version);
+  const newDoc = new TestDoc(doc.operations);
+  expect(newDoc.version).toEqual(doc.version);
+  expect(newDoc.getArray("arr").toJS()).toEqual(array.toJS());
+});
