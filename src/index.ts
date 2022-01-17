7dad72856982a4ceb49934427b950550064d90f0
DiamondYuan <admin@diamondyuan.com>
2022-01-17 23:28:17 +0800

    feat: export generateUuid and isUUID

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { DiamondDoc } from "./doc/DiamondDoc";
 export { DiamondArray } from "./structure/DiamondArray";
 export { DiamondMap } from "./structure/DiamondMap";
+export { generateUuid, isUUID } from "./base/uuid";
 export * from "./types";
 export * from "./constants";
